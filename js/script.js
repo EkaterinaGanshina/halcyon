ff0863b3025f09b7802e049659ea1547034f2388
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2018-07-28 00:50:39 +0300

    Finish mobile version

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Flickity( '#carousel-responsive', {
+  draggable: true,
+  autoPlay: 3000,
+  prevNextButtons: false
+});
+
+new Flickity( '#carousel-features', {
+  draggable: true,
+  prevNextButtons: false,
+  cellAlign: 'left'
+});
